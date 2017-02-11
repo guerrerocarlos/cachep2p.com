48f3ade24c6cf1ce1592f114e473a0a277b30153
guerrerocarlos@gmail.com <carlos@mediahint.com>
2017-02-11 23:33:52 +0200

    Update to latest webtorrent version

--- assets/js/cachep2p.security.js
+++ assets/js/cachep2p.security.js
@@ -1,5 +1,5 @@
 document.security_sha1 = {
-    'http://www.cachep2p.com/':                   '8666330fc1535a3af81afe4c1703e52d1021c031',
-    'http://www.cachep2p.com/documentation.html': '3f1bdb1b2ea2acce6647258138700eb3cafc17d3',
-    'http://www.cachep2p.com/api.html':       'fca113d8363b509dde72ed3eaa59fd14079de5d5'
+    'http://www.cachep2p.com/':                   'a654f038f48b1cdbaa3b01b0288565d5a8fd5e31',
+    'http://www.cachep2p.com/documentation.html': 'a6e6acf5ed05a9d33d41d37f0a0ee7fc7b26861b',
+    'http://www.cachep2p.com/api.html':       '7d5dcb865eed74efe05c08669753a9968469c0f6'
 };
--- assets/js/init.js
+++ assets/js/init.js
@@ -1,41 +1,49 @@
 var shell = Shell()
 var cachep2p = new CacheP2P()
 
-cachep2p.on('ready', function(message){
-    shell.draw()
-    shell.show()
+cachep2p.on('ready', function(message) {
+  shell.draw()
+  shell.show()
 })
-cachep2p.on('message', function(message){
-    shell.show({message: message, color: 'green'})
+cachep2p.on('message', function(message) {
+  shell.show({ message: message, color: 'green' })
 })
-cachep2p.on('alert', function(message){
-    shell.show({message: message, color: 'orange'})
+cachep2p.on('alert', function(message) {
+  shell.show({ message: message, color: 'orange' })
 })
-cachep2p.on('success', function(message){
-    shell.show({message: message, color: '#10d810'})
+cachep2p.on('success', function(message) {
+  shell.show({ message: message, color: '#10d810' })
 })
-cachep2p.on('webtorrent', function(message){
-    shell.show({message: message, color: '#a9a9a9'})
+cachep2p.on('webtorrent', function(message) {
+  shell.show({ message: message, color: '#a9a9a9' })
 })
-cachep2p.on('onpopstate', function(message){
-    var shell = Shell()
-    console.log('onpopstate from cachep2p')
-    shell.show({message: 'Showing Cached Page', color: 'blue'})
+cachep2p.on('onpopstate', function(message) {
+  var shell = Shell()
+  console.log('onpopstate from cachep2p')
+  shell.show({ message: 'Showing Cached Page', color: 'blue' })
 })
-cachep2p.on('cache', function(event){
-    var shell = Shell()
-    console.log('cache from cachep2p', event)
-    shell.clear()
-    shell.show({message: 'Showing page from cache: '+event.target.href+' (Not contacted the server)', color: '#43c6d2'})
+cachep2p.on('cache', function(event) {
+  var shell = Shell()
+  console.log('cache from cachep2p', event)
+  shell.clear()
+  shell.show({ message: 'Showing page from cache: ' + event.target.href + ' (Not contacted the server)', color: '#43c6d2' })
 })
-setInterval(function(){
-    shell.show()			
+
+setTimeout(function() {
+  shell.show()
 }, 1000)
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-86211205-1', 'auto');
 ga('send', 'pageview');
